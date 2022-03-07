e80fddbea79773c7c5a4ec8ab6b07bdddc0669f8
giacomo1235 <giacomo.passariello@gmail.com>
2022-03-07 15:13:31 +0100

    compilato array gente

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// const arrGente con dentro obj colleghi
+// for (key in arrGente ) { }
+
+const arrGente = [
+    {
+        img: 'wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+    },
+    {
+        img: 'walter-gordon-office-manager.jpg',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+    },
+    {
+        img: 'scott-estrada-developer.jpg',
+        name: 'Scott Estrada',
+        role: 'Developer',
+    },
+    {
+        img: 'barbara-ramos-graphic-designer.jpg',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+    },
+    {
+        img: 'angela-lopez-social-media-manager.jpg',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+    },
+    {
+        img: 'angela-caroll-chief-editor.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+    },
+
+];
+
+console.log(arrGente[1]['name'])
+// expected: 'Walter Gordon'
